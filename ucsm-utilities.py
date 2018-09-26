e97e26a85d037c2d4466733c1d84df10fc626928
pchi-macbook <peter.chi1@gmail.com>
2018-09-26 12:38:47 -0500

    created ucs backup functions - not tested

---
+++ ucsm-utilities.py
@@ -0,0 +1,32 @@
+#Import UCS modules
+from ucsmsdk.ucshandle import UcsHandle
+from ucsmsdk.utils.ucsbackup import backup_ucs
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", help="IP Address")
+    parser.add_argument("-u", "--username", help="Username")
+    parser.add_argument("-p", "--password", help="Password")
+
+    args = parser.parse_args()
+
+    #Log into UCS using UcsHandle
+    handle = UcsHandle(ip=args.ip, username=args.username, password=args.password)
+    handle.login()
+
+#Start backup scripts here
+def ucsmbackup():
+    backup_dir = "/home/user/backup"
+    full_state_backup_filename = "full-state_config_backup.xml"
+    backup_ucs(handle,
+               backup_type = "full-state",
+               file_dir = backup_dir,
+               file_name = full_state_backup_filename)
+
+    config_all_backup_filename = "config-all_config_backup,xml"
+    backup_ucs(handle,
+               backup_type = "config-all",
+               file_dir = backup_dir,
+               file_name = config_all_backup_filename,
+               preserve_pooled_values = True)
